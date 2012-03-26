315fff25a0fd106de7bb98a97a80bf372361830d
Nick Morgan <skilldrick@gmail.com>
2012-03-25 23:44:30 -0700

    Get rid of play all button. Refactor event handler setup.

--- tonegame.js
+++ tonegame.js
@@ -14,135 +14,17 @@
 
   $(setup);
 
+  //one-time setup
   function setup() {
-    setupButtons();
     updateScore(0);
+    setupHandlers();
     restart();
   }
 
-  window.restart = restart;
-
-  function restart(override) {
-    level = Math.floor(score / 50); //Increase level every 50 points
-    level = Math.max(0, level); //Keep level positive
-    level = Math.min(level, levels.length - 1); //Cap level at top
-    if (override) {
-      level = override;
-    }
-    updateScore(0);
-    scale = levels[level].scale;
-    audio.setupTones(scale);
-    var numberOfNotes = levels[level].notes;
-    chosenIndexes = pickDistinct(scale.length, numberOfNotes, chosenIndexes);
-    setupBoxes(scale.length);
-    $('#target .boxes .box').click(); //click to remove
-    var boxWidth = $('#scale .boxes .box').width();
-    $('#target .boxes').width(numberOfNotes * (boxWidth + 10));
-  }
-
-  function setupButtons() {
-    var $playAll = $('#playAll');
-    var $stop = $('#stop').hide();
-
-
-    $playAll.click(function () {
-      updateScore(-4);
-      playAll(function () {
-        $playAll.show();
-        $stop.hide();
-      });
-      $stop.show();
-      $playAll.hide();
-    });
-
-    $stop.click(function () {
-      audio.stop();
-      $('#scale .boxes .box').removeClass('playing');
-      $playAll.show();
-      $stop.hide();
-    });
-
+  function setupHandlers() {
     $('#clue .boxes').click(function () {
       playClue();
     });
-  }
-
-  function updateScore(by) {
-    score += by;
-    $('#score').text(score);
-    $('#level').text(level);
-  }
-
-  function playClue() {
-    audio.play(chosenIndexes, function () {
-      $('#clue').addClass('playing');
-    }, function () {
-      $('#clue').removeClass('playing');
-    });
-  }
-
-  function playGuess() {
-    var guessedIndexes = $('#target .box').map(function () {
-      return $(this).data('scale-index');
-    }).toArray();
-    if (guessedIndexes.length) {
-      updateScore(-2);
-      audio.playGuess(guessedIndexes, function () {
-        $('#target').addClass('playing');
-      }, function () {
-        $('#target').removeClass('playing');
-        winning(guessedIndexes);
-      });
-    }
-  }
-
-  function winning(guessedIndexes) {
-    if (sameArray(chosenIndexes, guessedIndexes)) {
-      updateScore((level * 2) + 12);
-      $('#cross').hide();
-      $('#tick').fadeIn(200).delay(1000).fadeOut(1000);
-      restart();
-    } else {
-      $('#tick').hide();
-      $('#cross').fadeIn(200).delay(1000).fadeOut(1000);
-    }
-  }
-
-  function setupBoxes(count) {
-    $('#target .boxes').off();
-    $('#clue .boxes').empty();
-    var $boxes = $('#scale .boxes').empty();
-    var $box;
-    var hue;
-    var saturation;
-    var octave;
-    for (var i = 0; i < numberOfNotes; i++) {
-      $box = $('<span>').attr('class', 'box box-' + i);
-      $box.attr('data-index', i);
-      hue = (360 / 12) * i
-      octave = Math.floor(i / 12);
-      saturation = (octave === 0) ? 80 : 60;
-      $box.css('background-color', "hsla(" + hue + ", " + saturation + "%, 50%, 1)");
-      $boxes.append($box);
-      if (scale.indexOf(i) > -1) { //this is a note
-        $box.attr('data-scale-index', scale.indexOf(i));
-        $box.addClass('in-scale');
-        $box.attr('draggable', 'true');
-        $box.on('dragstart', function (e) {
-          e.originalEvent.dataTransfer.effectAllowed = 'move';
-          e.originalEvent.dataTransfer.setData('Text', $(this).attr('data-index'));
-        });
-        $box.click(function () {
-          updateScore(-1);
-          var scaleIndex = $(this).data('scale-index');
-          playAndHighlight(scaleIndex);
-        });
-      }
-    }
-
-    for (var i = 0; i < chosenIndexes.length; i++) {
-      $('#clue .boxes').append('<div class="box"><p class="question-mark">?</p></div>');
-    }
 
     $('#target .boxes').on('dragover', function (e) {
       e.preventDefault(); // allows us to drop
@@ -185,18 +67,107 @@
     });
   }
 
-  function playAll(allDone) {
-    var index = 0;
+  //Give console access to restart
+  window.restart = restart;
 
-    function playNext() {
-      if (index < scale.length) {
-        playAndHighlight(index, playNext);
-        index++;
-      } else {
-        allDone && allDone();
+  //run this for each new clue
+  function restart(override) {
+    level = Math.floor(score / 50); //Increase level every 50 points
+    level = Math.max(0, level); //Keep level positive
+    level = Math.min(level, levels.length - 1); //Cap level at top
+    if (override) {
+      level = override;
+    }
+    updateScore(0);
+    scale = levels[level].scale;
+    audio.setupTones(scale);
+    var numberOfNotes = levels[level].notes;
+    chosenIndexes = pickDistinct(scale.length, numberOfNotes, chosenIndexes);
+    setupScaleBoxes();
+    setupClueBoxes();
+    $('#target .boxes .box').click(); //click to remove
+    var boxWidth = $('#scale .boxes .box').width();
+    $('#target .boxes').width(numberOfNotes * (boxWidth + 10));
+  }
+
+  function updateScore(by) {
+    score += by;
+    $('#score').text(score);
+    $('#level').text(level);
+  }
+
+  function playClue() {
+    audio.play(chosenIndexes, function () {
+      $('#clue').addClass('playing');
+    }, function () {
+      $('#clue').removeClass('playing');
+    });
+  }
+
+  function playGuess() {
+    var guessedIndexes = $('#target .box').map(function () {
+      return $(this).data('scale-index');
+    }).toArray();
+    if (guessedIndexes.length) {
+      audio.playGuess(guessedIndexes, function () {
+        $('#target').addClass('playing');
+      }, function () {
+        $('#target').removeClass('playing');
+        updateScore(-2);
+        winning(guessedIndexes);
+      });
+    }
+  }
+
+  function winning(guessedIndexes) {
+    if (sameArray(chosenIndexes, guessedIndexes)) {
+      updateScore((level * 2) + 12);
+      $('#cross').hide();
+      $('#tick').fadeIn(200).delay(1000).fadeOut(1000);
+      restart();
+    } else {
+      $('#tick').hide();
+      $('#cross').fadeIn(200).delay(1000).fadeOut(1000);
+    }
+  }
+
+  function setupScaleBoxes() {
+    var $boxes = $('#scale .boxes').empty();
+    var $box;
+    var hue;
+    var saturation;
+    var octave;
+    for (var i = 0; i < numberOfNotes; i++) {
+      $box = $('<span>').attr('class', 'box box-' + i);
+      $box.attr('data-index', i);
+      hue = (360 / 12) * i
+      octave = Math.floor(i / 12);
+      saturation = (octave === 0) ? 80 : 60;
+      $box.css('background-color', "hsla(" + hue + ", " + saturation + "%, 50%, 1)");
+      $boxes.append($box);
+      if (scale.indexOf(i) > -1) { //this is a note
+        $box.attr('data-scale-index', scale.indexOf(i));
+        $box.addClass('in-scale');
+        $box.attr('draggable', 'true');
+        $box.on('dragstart', function (e) {
+          e.originalEvent.dataTransfer.effectAllowed = 'move';
+          e.originalEvent.dataTransfer.setData('Text', $(this).attr('data-index'));
+        });
+        $box.click(function () {
+          updateScore(-1);
+          var scaleIndex = $(this).data('scale-index');
+          playAndHighlight(scaleIndex);
+        });
       }
     }
-    playNext();
+  }
+
+  function setupClueBoxes() {
+    $('#clue .boxes').empty();
+
+    for (var i = 0; i < chosenIndexes.length; i++) {
+      $('#clue .boxes').append('<div class="box"><p class="question-mark">?</p></div>');
+    }
   }
 
   function playAndHighlight(index, callback) {
